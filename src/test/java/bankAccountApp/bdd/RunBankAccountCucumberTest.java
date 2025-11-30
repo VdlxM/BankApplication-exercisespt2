fb5f1e5dcd519d56072946f43ca186573f631c8c
VdlxM <matt.vdlx@gmail.com>
2025-11-30 22:27:17 +0100

    Cucumber tests

---
+++ src/test/java/bankAccountApp/bdd/RunBankAccountCucumberTest.java
@@ -0,0 +1,14 @@
+package bankAccountApp.bdd;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "bankAccountApp.bdd",
+        plugin = { "pretty" }
+)
+public class RunBankAccountCucumberTest {
+    // Empty on purpose. JUnit + Cucumber will use the annotations above.
+}
